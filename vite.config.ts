f91a162ead64ba3d029667cfb647cebbe7d579a7
anderdc <alexanderdcastaneda@gmail.com>
2024-05-17 23:15:31 -0500

    dockerized project

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 8080,
+   strictPort: true,
+  },
+  server: {
+   port: 8080,
+   strictPort: true,   //Set to true to exit if the port is already in use, instead of automatically trying the next available port.
+   host: true,    //set to true to make the server accessible on your network 
+   origin: "http://127.0.0.1:8080",
+  },
+ });
